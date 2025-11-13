d0f96322058dd06651077a8fae6ce25695b3424a
kika1s1 <kika1s1>
2025-11-13 22:00:00 +0300

    feat(admin): add lib utilities

---
+++ admin/lib/adRegion.ts
@@ -0,0 +1,13 @@
+export const adRegions: Record<string, [number, number]> = {
+    "Addis Ababa": [38.7469, 9.03],
+    "Adama": [39.27, 8.54],
+    "Bahir Dar": [37.3833, 11.6],
+    "Hawassa": [38.5126, 7.0621],
+    "Mekelle": [39.4700, 13.4967],
+    "Dire Dawa": [41.8661, 9.5931],
+    "Jimma": [36.8333, 7.6667],
+    "Gondar": [37.4667, 12.6],
+    "Dessie": [39.6333, 11.1333],
+    "Shashamane": [38.5833, 7.2000]
+  };
+  
