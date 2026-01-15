ec29085eb5544e6f460505a78750a13831dc2e46
chera-mihiretu <cheramihiretucm@gmail.com>
2026-01-15 20:04:53 +0300

    Update functions.js

--- web/libs/functions.js
+++ web/libs/functions.js
@@ -8,7 +8,7 @@ import { getServerSession } from "next-auth";
 import { options } from "@/app/api/auth/[...nextauth]/options";
 import { sendEmail } from "./sendEmail";
 import { Agenda } from "agenda";
-import { getIO } from "../socket-server/libs/socket"
+import { getIO } from "./socket"
 import jwt from "jsonwebtoken"
 
 const agenda = new Agenda({ db: { address: process.env.MONGO_URL } });
